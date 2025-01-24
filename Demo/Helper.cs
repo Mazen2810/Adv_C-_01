8e6229facaef224bfe9ba8b440d2e9b9941640af
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-24 13:35:53 +0200

    Generics - SWAP Example 01

---
+++ Demo/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal static class Helper
+    {
+        public static void SWAP<T>(ref T X , ref T Y)
+        {
+            Console.WriteLine("******* SWAP *******");
+            T Temp = X;
+            X = Y;
+            Y=Temp;
+        }
+
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Generics - SWAP Example 01
+            /// int A = 3, B = 5;
+            /// Console.WriteLine($"A = {A}");
+            /// Console.WriteLine($"B = {B}");
+            /// Helper.SWAP/*<int>*/(ref A, ref B);
+            /// Console.WriteLine($"A = {A}");
+            /// Console.WriteLine($"B = {B}");
+
+            /// double K = 3.3, L = 4.4;
+            /// Console.WriteLine($"k = {K}");
+            /// Console.WriteLine($"L = {L}");
+            /// Helper.SWAP/*<double>*/(ref K, ref L);
+            /// Console.WriteLine($"k = {K}");
+            /// Console.WriteLine($"L = {L}");
+
+
+            /// Point P01 = new Point(10, 20);
+            /// Point P02 = new Point(50, 60);
+            /// Console.WriteLine($"P01 = {P01}");
+            /// Console.WriteLine($"P02 = {P02}");
+            /// Helper.SWAP/*<Point>*/(ref P01, ref P02);
+            /// Console.WriteLine($"P01 = {P01}");
+            /// Console.WriteLine($"P02 = {P02}"); 
+            #endregion
+
         }
     }
 }
