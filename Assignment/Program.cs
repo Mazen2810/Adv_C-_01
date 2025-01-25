64c7a4fedb36b8e2502d0d81a73e738e7070e6e8
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-26 00:54:30 +0200

    Q3

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using Assignment.Q2;
+using Assignment.Q3;
 
 namespace Assignment
 {
@@ -28,6 +29,30 @@ namespace Assignment
             // StringRange.IsInRange("Mazen");  // True
             // StringRange.IsInRange("Ziad");   // False 
             #endregion
+
+            #region Q3: try to reverse the order of elements in the ArrayList in-place
+            /// int[] Numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+            /// 
+            /// Reverse.reverse(Numbers, delegate (int X, int Y)
+            /// {
+            ///     return X.CompareTo(Y) == -1;   // it will reverse accodring to the delegate
+            /// });
+            /// 
+            /// foreach (int number in Numbers)
+            ///     Console.WriteLine(number);
+
+
+            ///  string[] Names = ["Ahmed", "Bassem", "Menna", "Yasser", "Ziad"];
+            /// 
+            ///  Reverse.reverse(Names, delegate (string X, string Y)
+            ///  {
+            ///      return X.CompareTo(Y) == -1;  // it will reverse accodring to the delegate
+            ///  });
+            /// 
+            ///  foreach (string name in Names)
+            ///     Console.WriteLine(name); 
+            #endregion
+
         }
     }
 }
---
+++ Assignment/Q3/Reverse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Q3
+{
+    internal class Reverse
+    {
+
+        public static void reverse<T>(T [] Arr , Func<T , T , bool> comparer) where T : IComparable<T>
+        {
+            for (int i = 0; i < Arr.Length; i++)
+            {
+
+                for (int j = 0; j < Arr.Length - 1 - i; j++)
+                {
+                    if (comparer(Arr[j] , Arr[j+1]))
+                    {
+                        T Temp = Arr[j];
+                        Arr[j] = Arr[j + 1];
+                        Arr[j + 1] = Temp;
+                    }
+                }
+            }
+        }
+    }
+}
