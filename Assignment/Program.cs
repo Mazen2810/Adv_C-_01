b1df708678fd3a6047cbc7613ab2976d08910408
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-26 00:19:17 +0200

    Q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Assignment
+﻿
+using Assignment.Q2;
+
+namespace Assignment
 {
     internal class Program
     {
@@ -10,6 +13,20 @@
 
             //foreach (string Name in Names)
             //    Console.WriteLine(Name); 
+            #endregion
+
+
+            #region Q2: create a generic Range<T> class
+            // Assignment.Q2.Range<int> NumRange = new Assignment.Q2.Range<int>(20, 10);
+            //
+            // NumRange.IsInRange(15);  // True
+            // NumRange.IsInRange(25);  // False
+
+
+            // Assignment.Q2.Range<string> StringRange = new Assignment.Q2.Range<string>("Ahmed", "Yasser");
+            //
+            // StringRange.IsInRange("Mazen");  // True
+            // StringRange.IsInRange("Ziad");   // False 
             #endregion
         }
     }
---
+++ Assignment/Q2/Range.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Q2
+{
+    internal class Range<T> where T : IComparable<T>
+    {
+
+        public T Max { get; set; }
+        public T Min { get; set; }
+        public Range(T Value01 , T Value02)
+        {
+            if(Value01.CompareTo(Value02) > 0)
+            {
+                Max = Value01;
+                Min = Value02;
+            }
+            else
+            {
+                Max = Value02;
+                Min = Value01;
+            }
+        }
+
+        public void IsInRange(T Value)
+        {
+            if(Value.CompareTo(Min)> 0 && Value.CompareTo(Max) < 0)
+                Console.WriteLine("True");
+            
+            else
+                Console.WriteLine("False");
+        }
+
+        ///public T Length()
+        ///{
+        ///    return 
+        ///}
+    }
+}
