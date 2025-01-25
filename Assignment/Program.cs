f55bbe41c9ca2704da1f15186172bd7a53e5e627
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-25 23:16:09 +0200

    Q1

---
+++ Assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1: Bubble Sort algorithm
+            //string[] Names = ["Mazen", "Ahmed", "Ziad", "Hassan"];
+            //Utility.BubbleSort(Names);
+
+            //foreach (string Name in Names)
+            //    Console.WriteLine(Name); 
+            #endregion
+        }
+    }
+}
